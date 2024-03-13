4d98b6213480a3c3b7f444fed28c8d52672cc065
Shrikrushna Ade <shrikrushnaade766@gmail.com>
2024-03-13 14:21:32 +0530

    AuthLayout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -45,6 +45,34 @@ function Login() {
           </Link>
         </p>
         {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+        <form onSubmit={handleSubmit(login)} className='mt-8' >
+        <div className='space-y-5'>
+                <Input
+                label="Email: "
+                placeholder="Enter your email"
+                type="email"
+                {...register("email", {
+                    required: true,
+                    validate: {
+                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                        "Email address must be a valid address",
+                    }
+                })}
+                />
+                <Input
+                label="Password: "
+                type="password"
+                placeholder="Enter your password"
+                {...register("password", {
+                    required: true,
+                })}
+                />
+                <Button
+                type="submit"
+                className="w-full"
+                >Sign in</Button>
+            </div>
+        </form>
         
       </div>
     </div>
